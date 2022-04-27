ebe828925d70e61852d4cbf8889a5149373c3ea6
aileenlap <101245773+aileenlap@users.noreply.github.com>
2022-04-27 09:00:12 +0200

    healthbar

--- Slime.gd
+++ Slime.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var health = 100 setget change_health
-export var max_health = 50
+export var max_health = 35
 
 func change_health (new_life):
 	health = new_life
@@ -11,13 +11,12 @@ func change_health (new_life):
 	$AnimatedSprite.play("idle")
 	if self.health == 0:
 		$AnimatedSprite.play('die')
-		yield(get_tree().create_timer(1.2), "timeout")
-		position = Vector2(100000,100000)
+		yield(get_tree().create_timer(0.6), "timeout")
+		position = Vector2(10000,10000)
 
 func _ready():
 	$HealthBar/TextureProgress.max_value = max_health
 	$HealthBar/TextureProgress.value = health
 	$AnimatedSprite.play("idle")
 	yield(get_tree().create_timer(3), 'timeout')
-	self.health = 40
-
+	self.health = 0
