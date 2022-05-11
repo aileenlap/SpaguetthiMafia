dc36be3d38a82b3a8fe057c8d681e8cd3c2292a1
aileenlap <101245773+aileenlap@users.noreply.github.com>
2022-05-11 08:55:26 +0200

    atac, vida

--- Personatge.gd
+++ Personatge.gd
@@ -2,11 +2,13 @@ extends KinematicBody2D
 
 var health = 100 setget change_health
 export var max_health = 50
+signal atac(damage_totalp)
 
 func change_health (new_life):
 	health = new_life
-	$HealthBar._on_health_updated(new_life)
+	yield(get_tree().create_timer(1.5), "timeout")
 	$AnimatedSprite.play("hurt")
+	$HealthBar._on_health_updated(new_life)
 	yield(get_tree().create_timer(0.8), 'timeout')
 	$AnimatedSprite.play("idle")
 	if self.health == 0:
@@ -26,23 +28,30 @@ func _ready():
 	rng.randomize()
 	var my_random_number = rng.randi_range(1, 3)
 	if my_random_number == 1:
-		_attack(10,2)
+		_attack(20,2)
 	elif my_random_number == 2:
-		_attack2(10,2)
+		_attack2(20,2)
 	else:
-		_attack3(10,2)
+		_attack3(20,2)
 	
 func _attack(damage, buff):
 	$AnimationPlayer.play("attack")
-	var damage_total = damage + buff
-	return damage_total
+	var damage_totalp = damage + buff
+	emit_signal("atac", damage_totalp)
+	return damage_totalp
 	
 func _attack2(damage, buff):
 	$AnimationPlayer.play("attack2")
-	var damage_total = damage + buff
-	return damage_total
+	var damage_totalp = damage + buff
+	emit_signal("atac", damage_totalp)
+	return damage_totalp
 	
 func _attack3(damage, buff):
 	$AnimationPlayer.play("attack3")
-	var damage_total = damage + buff
-	return damage_total
+	var damage_totalp = damage + buff
+	emit_signal("atac", damage_totalp)
+	return damage_totalp
+
+
+func _on_Slime_atac(damage_totals):
+	self.health -= damage_totals
--- Slime.gd
+++ Slime.gd
@@ -1,12 +1,20 @@
 extends KinematicBody2D
 
 var health = 100 setget change_health
-export var max_health = 35
+export var max_health = 35 setget change_max
+signal atac(damage_totals)
 
+func change_max(new_max):
+	if health > new_max:
+		health = new_max
+	max_health = new_max
+	$HealthBar/TextureProgress.max_value = max_health
+	
 func change_health (new_life):
 	health = new_life
-	$HealthBar._on_health_updated(new_life)
+	yield(get_tree().create_timer(1.5), 'timeout')
 	$AnimatedSprite.play("hurt")
+	$HealthBar._on_health_updated(new_life)
 	yield(get_tree().create_timer(0.8), 'timeout')
 	$AnimatedSprite.play("idle")
 	if self.health == 0:
@@ -15,16 +23,18 @@ func change_health (new_life):
 		position = Vector2(10000,10000)
 
 func _ready():
-	$HealthBar/TextureProgress.max_value = max_health
-	$HealthBar/TextureProgress.value = health
+	change_max(max_health)
+	change_health(health)
 	$AnimatedSprite.play("idle")
 	yield(get_tree().create_timer(3), 'timeout')
-	_attack(5)
-#	self.health = 0
-	var damages = 0
-#	damages.get_node('damage_total').
+#	_attack(5)
 
 func _attack(damage):
 	$AnimationPlayer.play('attack')
-	var damage_total = damage
-	return damage_total
+	var damage_totals = damage
+	emit_signal("atac", damage_totals)
+	return damage_totals
+
+
+func _on_Personatge_atac(damage_totalp):
+	self.health = health - damage_totalp
