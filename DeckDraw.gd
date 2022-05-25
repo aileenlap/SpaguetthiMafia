6c50b8115b407d4e862f752aca935251d505d59a
Roger Garcia <101257233+r0gergarc1a@users.noreply.github.com>
2022-05-25 08:38:05 +0200

    Diu que diu click! (o no)

--- DeckDraw.gd
+++ DeckDraw.gd
@@ -8,6 +8,7 @@ func _ready():
 	
 func _gui_input(event):
 	if Input.is_action_just_released("click"):
+		print('Click!')
 		if DeckSize > 0:
 			DeckSize = $'../../'.drawcard()
 			if DeckSize == 0:
